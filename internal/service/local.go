c3e8de6e18df14a89e62064fd4b8b3710c6f19fa
aa <1061838259@qq.com>
2026-04-09 19:04:18 +0800

    feat: 打印对齐

--- internal/service/local.go
+++ internal/service/local.go
@@ -79,7 +79,7 @@ func (l *Local) RecvMeta(meta *sync.Meta) bool {
 	if !ok {
 		return false
 	}
-	log.Printf("[INFO] 📡 receive meta %s", meta.Digest)
+	log.Printf("[INFO] 📡 receive meta  %s", meta.Digest)
 	data := l.cache.Get(meta.Digest)
 	if data == nil {
 		return true
@@ -99,7 +99,7 @@ func (l *Local) RecvData(data *clipboard.Data) {
 	if meta == nil {
 		return
 	}
-	log.Printf("[INFO] 📥 receive data %s", meta.Digest)
+	log.Printf("[INFO] 📥 receive data  %s", meta.Digest)
 	// data在更新列中存在，写入缓存，写入剪贴板，推送给所有peer
 	l.cache.Put(data)
 	l.WriteToClipboard(data)
